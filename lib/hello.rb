aadcedab9dca782108f1720c9525867d57f374c1
Lorena <lorena.giddens@outlook.com>
2018-06-06 01:40:55 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,18 +1,26 @@
 # 1: give method an argument
 def hello_t(array)
-  # 2: enact an iteration with while
-  i = 0
+  # refactor #hello_t so that it can be called either with or without a block
+  if block_given?
+    # 2: enact an iteration with while
+    i = 0
 
-  while i < array.length
-    # 3: yield to the block
-    yield array[i]
-    i = i + 1
+    while i < array.length
+      # 3: yield to the block
+      yield(array[i])
+      i = i + 1
+    end
+    # 5: pass the test by letting #hello_t return the original array
+    array
+  # refactor #hello_t so that it can be called either with or without a block
+  else
+    puts "Hey! No block was given!"
   end
 end
 
 # 4: call the method
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
