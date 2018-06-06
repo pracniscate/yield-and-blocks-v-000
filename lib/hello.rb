7ba0757c17c9274c33bf1fb23b2886f380778490
Lorena <lorena.giddens@outlook.com>
2018-06-06 01:33:19 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
+# 1: give method an argument
+def hello_t(array)
+  # 2: enact an iteration with while
+  i = 0
 
+  while i < array.length
+    # 3: yield to the block
+    yield array[i]
+    i = i + 1
+  end
 end
 
-# call your method here!
-
+# 4: call the method
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
